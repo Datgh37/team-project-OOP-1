12e8a4b7a3f90cf5115c911c4a62500aad28c5f7
Datgh37 <thanhdatgh37@gmail.com>
2025-10-17 15:12:36 +0700

    Update FormCustomer

---
+++ FormCustomer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankManagement
+{
+    public partial class FormCustomer : Form
+    {
+        public FormCustomer()
+        {
+            InitializeComponent();
+            // Ẩn nút phóng to
+            this.MaximizeBox = false;
+
+            // (tuỳ chọn) không cho resize
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            using (FormEdit f = new FormEdit())   // mở form ở chế độ Edit
+            {
+                if (f.ShowDialog() == DialogResult.OK)
+                {
+                    
+                }
+            }
+        }
+    }
+}
